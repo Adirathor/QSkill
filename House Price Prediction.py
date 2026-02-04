e46565a07ab0bde9cf5957d9de0b0372a9a27ad4
Adirathor <adirathor0962@gmail.com>
2026-02-04 17:42:54 +0530

    Add house price prediction model and dataset

---
+++ House Price Prediction.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+df = pd.read_csv("house_price_regression_dataset.csv")
+print(df.head()) 
+df = df.dropna()
+X = df[['Rooms','Size','Location']]
+y = df['Price']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+print("MAE:", mean_absolute_error(y_test, y_pred))
+print("MSE:", mean_squared_error(y_test, y_pred))
+print("R2 Score:", r2_score(y_test, y_pred))
+new_house = [[3,1200,2]]
+predicted_price = model.predict(new_house)
+print("Predicted Price:", predicted_price)
+
